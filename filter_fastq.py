a268b6c93b456ced791b6379e7417bb3c635f35d
daria nikanorova <daria.nikanorova@mail.ru>
2021-02-01 22:27:08 +0300

    Updated errors catching and added help page

---
+++ filter_fastq.py
@@ -0,0 +1,37 @@
+import arg_parsing as arp
+from sys import argv
+
+# ARGUMENTS PARSING
+
+# the arguments are stored in sys.argv
+unparsed_args = argv
+
+# a dict that contains all optional arguments and their values by default
+default_args = {
+    '--min_length': 0,
+    '--gc_bounds': [0.0, 100.0],
+    '--keep_filtered': False,
+    '--output_base_name': ''
+}
+
+# parse optional arguments, values will be stored in parsed_args
+parsed_args = arp.parse_args(unparsed_args, default_args.copy())
+
+# args after parsing can be found in parsed_args:
+fastq_file = parsed_args[1]
+optional_parsed_args = parsed_args[0]
+
+min_length = optional_parsed_args['--min_length']
+gc_bounds = optional_parsed_args['--gc_bounds']
+min_gc_bound = optional_parsed_args['--gc_bounds'][0]
+max_gc_bound = optional_parsed_args['--gc_bounds'][1]
+keep_filtered = optional_parsed_args['--keep_filtered']
+output_base_name = optional_parsed_args['--output_base_name']
+
+print(f'\n{fastq_file} will be filtered with further parameters: \n'
+      f'--min_length = {min_length}bp, \n'
+      f'--gc_bounds = {min_gc_bound}% - {max_gc_bound}%, \n'
+      f'--keep_filtered = {keep_filtered},\n'
+      f'--output_base_name = {output_base_name}')
+
+print(optional_parsed_args)
